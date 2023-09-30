002c4dd93c2fe6c90686758cc890dfb96f623295
rob <rowntreerob@gmail.com>
2023-09-30 07:56:27 -0700

    upgrad roboflow model call to V3

--- app.js
+++ app.js
@@ -49,7 +49,7 @@ function wrapAsync (callback) {
 }
 // TODO config domain 
 async function getClass(base64) {
-	let res = await axios.post(`https://classify.roboflow.com/org311-clip-photos/2?api_key=${ROBOFLOWKEY}`
+	let res = await axios.post(`https://classify.roboflow.com/org311-clip-photos/3?api_key=${ROBOFLOWKEY}`
 		,base64 
 		, { headers: {'Content-Type': 'application/x-www-form-urlencoded'}
 	})
